194bc4a471722d1579557b1b830af3b7d6167f51
Ryan Moon <ryanmoon@dynamic-oit-ip4-wifirestricted03-10-48-107-223.princeton.edu>
2026-01-20 10:41:01 -0500

    now analyzes simplified binary tree

--- data_analysis_code/TMD_analysis.py
+++ data_analysis_code/TMD_analysis.py
@@ -38,20 +38,12 @@ def sort_persistence_pairs(coords):
     2) increasing coordinate magnitude
     """
     
-    return sorted(coords, key=lambda x: (-(x[1] - x[0]), x[0] + x[1]))
-
-def color_plot_walk(G, TMD_coords, fly, side):
-
-    savename = f"TMD_data/image_{fly}_{side}"
-    fig, axs = plt.subplots(2, 2)
+    for coord in coords:
+        print(coord)
     
-    ax1 = axs[0, 0]
-    ax2 = axs[0, 1]
-    ax3 = axs[1, 0]
-    ax4 = axs[1, 1]
-    ax4.axis("off")
-    ax1.text(0.05, 0.95, f'{fly}_{side}', transform=ax1.transAxes, fontsize=17, va='center_baseline', ha='center')
-    
+    return sorted(coords, key=lambda x: (abs(x[1] - x[0]), x[0] + x[1]))
+
+def draw_G(G, ax):
     maxLevel = max(G.nodes()) + 1
     palette = sns.dark_palette("red", maxLevel, reverse=True)
     
@@ -67,7 +59,7 @@ def color_plot_walk(G, TMD_coords, fly, side):
         # the level gives index for RGB value
         c = palette[e[0]]
              
-        ax1.plot([c0[0], c1[0]], [c0[1], c1[1]], color=c, linewidth = 1)
+        ax.plot([c0[0], c1[0]], [c0[1], c1[1]], color=c, linewidth = 1)
 
     trace_coords = np.array([i[:2] for i in nx.get_node_attributes(G, 'coords').values()])
     xs = trace_coords[:, 0]
@@ -78,29 +70,41 @@ def color_plot_walk(G, TMD_coords, fly, side):
 
     lim = 300
 
-    ax1.axis([xcent - lim, xcent + lim, ycent - lim, ycent + lim])
+    ax.axis([xcent - lim, xcent + lim, ycent - lim, ycent + lim])
     plt.gca().set_aspect('equal', adjustable='box')
-    ax1.axis('off')
+    ax.axis('off')
+
+def show_data(G, cut_G, TMD_coords, fly, side):
+
+    savename = f"TMD_data/image_{fly}_{side}"
+    fig, axs = plt.subplots(2, 2)
     
+    ax1 = axs[0, 0]
+    ax2 = axs[0, 1]
+    ax3 = axs[1, 0]
+    ax4 = axs[1, 1]
+    ax2.axis("off")
+    ax1.text(0.05, 0.95, f'{fly}_{side}', transform=ax1.transAxes, fontsize=17, va='center_baseline', ha='center')
+    
+    draw_G(G, ax1)
+    draw_G(cut_G, ax2)
+        
     # for the persistence diagram
-    ax2.axis(give_axes(TMD_coords)) # add a formatting function here that dynamically chooses axes
-    ax2.axis("on")
-    ax2.set_xlabel("Birth (distance from root)", loc="left")
-    ax2.set_ylabel("Death (dist from root)")
-    ax2.grid()
+    ax4.axis(give_axes(TMD_coords)) # add a formatting function here that dynamically chooses axes
+    ax4.axis("on")
+    ax4.set_xlabel("Birth (distance from root)", loc="left")
+    ax4.set_ylabel("Death (dist from root)")
+    ax4.grid()
     
     for x, y in TMD_coords:
-        ax2.scatter(x, y, color="red", s=10)
+        ax4.scatter(x, y, color="red", s=10)
         
     # persistence barcode
-    ax3.axis(give_axes(TMD_coords, barcode=True)) # add a formatting function here that dynamically chooses axes
+    ax3.axis(give_axes(TMD_coords, barcode=True))
     ax3.axis("on")
     ax3.set_xlabel("Lifetime (distance from root)")
     ax3.set_ylabel("Length of Lifetime")
     ax3.grid()
-    
-    # y axis is in increasing length of strand
-    TMD_coords = sort_persistence_pairs(TMD_coords)
 
     for index in range(len(TMD_coords)):
         birth, death = TMD_coords[index]
@@ -108,19 +112,6 @@ def color_plot_walk(G, TMD_coords, fly, side):
 
     plt.savefig(savename + '.pdf', bbox_inches='tight')
     plt.close()
-    
-# def get_closest_node(G, n_coords):
-
-#     all_pts = nx.get_node_attributes(G, 'coords')
-
-#     dist_dict = {k: np.sqrt(np.sum((all_pts[k] - n_coords)**2)) for k in all_pts.keys()}
-
-#     sorted_by_dist = dict(sorted(dist_dict.items(), key=lambda item: item[1]))
-#     #closest = sorted_by_dist[0]
-
-#     #print(list(sorted_by_dist.keys())[:5])
-#     return list(sorted_by_dist.keys())[0]
-
 
 def get_closest_node(G, coords, exclude_nodes=None):
     if exclude_nodes is None:
@@ -141,7 +132,9 @@ def get_closest_node(G, coords, exclude_nodes=None):
     return best[1]
 
 
-def trace_file_to_G(filename):
+def TFG(filename):
+    '''Takes in a .trace file, returns a DiGraph object constructed from trace
+    TFG is short for Trace File to Graph'''
 
     input = gzip.open(filename, 'r')
     tree = ET.parse(input)
@@ -177,7 +170,6 @@ def trace_file_to_G(filename):
             zd = np.round(float(nDict[keys[5]]), 4)
             r = float(nDict[keys[0]])
     
-    
             nodes.append({
                 "id": node_count,
                 "coords": np.array((xd, yd, zd)),
@@ -193,7 +185,6 @@ def trace_file_to_G(filename):
     
     for n in nodes:
         G.add_node(n["id"], coords=n["coords"], radius=n["radius"])
-
 
     for i, n in enumerate(nodes):
         if i == 0:
@@ -219,6 +210,120 @@ def trace_file_to_G(filename):
         c1 = np.array(G.nodes[e[0]]['coords'][:2])
         c2 = np.array(G.nodes[e[1]]['coords'][:2])
         G[e[0]][e[1]]['length'] = np.sqrt((c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2)
+    
+    # cut out the degree 2 nodes
+    cut_G = copy.deepcopy(G)
+    more_to_cut = True
+    while more_to_cut:
+        more_to_cut = False
+        # if there are no more degree 2 nodes, more_to_cut will stay False and exit loop
+        for n in list(cut_G.nodes()):
+            if cut_G.in_degree(n) == 1 and cut_G.out_degree(n) == 1:
+                parent = list(cut_G.predecessors(n))[0]
+                child  = list(cut_G.successors(n))[0]
+
+                # accumulate length
+                new_len = cut_G[parent][n]['length'] + cut_G[n][child]['length']
+
+                # reconnect
+                cut_G.add_edge(parent, child, length=new_len)
+
+                # remove middle node
+                cut_G.remove_node(n)
+                more_to_cut = True
+                break
+
+    return G, cut_G
+
+def trace_file_to_G(filename):
+
+    input = gzip.open(filename, 'r')
+    tree = ET.parse(input)
+    root = tree.getroot()
+
+    G = nx.DiGraph()
+    node_count = 1
+
+    nodes = []   # (id, coords, radius, path_id, is_first)
+
+    # ---------- PASS 1: read ALL points ----------
+    for path_id, path in enumerate(root):
+        if path.tag != "path":
+            continue
+        if path.attrib["usefitted"] != "false":
+            continue
+
+        first = True
+        for n in path.iter(tag="point"):
+            nDict = n.attrib
+            keys = list(nDict.keys())
+
+            xd = np.round(float(nDict[keys[3]]), 4)
+            yd = np.round(float(nDict[keys[4]]), 4)
+            zd = np.round(float(nDict[keys[5]]), 4)
+            r  = float(nDict[keys[0]])
+
+            nodes.append({
+                "id": node_count,
+                "coords": np.array((xd, yd, zd)),
+                "radius": r,
+                "path_id": path_id,
+                "is_first": first
+            })
+
+            first = False
+            node_count += 1
+
+    # ---------- ADD ALL NODES ----------
+    for n in nodes:
+        G.add_node(n["id"], coords=n["coords"], radius=n["radius"])
+
+    # ---------- PASS 2: connect full paths ----------
+    for i, n in enumerate(nodes):
+        if i == 0:
+            continue
+
+        if not n["is_first"]:
+            # extension along same path
+            G.add_edge(nodes[i - 1]["id"], n["id"])
+        else:
+            # branch: connect first point of path to closest existing node
+            exclude = {m["id"] for m in nodes if m["path_id"] == n["path_id"]}
+            parent = get_closest_node(G, n["coords"], exclude_nodes=exclude)
+            G.add_edge(parent, n["id"])
+
+    # ---------- FLIP Y AXIS ----------
+    coords = nx.get_node_attributes(G, 'coords')
+    max_y = np.max([v[1] for v in coords.values()])
+    coords = {k: (coords[k][0], max_y - coords[k][1], coords[k][2]) for k in coords}
+    nx.set_node_attributes(G, coords, 'coords')
+
+    # ---------- EDGE LENGTHS ----------
+    for u, v in G.edges():
+        c1 = np.array(G.nodes[u]['coords'][:2])
+        c2 = np.array(G.nodes[v]['coords'][:2])
+        G[u][v]['length'] = np.linalg.norm(c1 - c2)
+
+    # ---------- PASS 3: collapse degree-2 nodes ----------
+    more_to_cut = True
+    while more_to_cut:
+        more_to_cut = False
+        # if there are no more degree 2 nodes, more_to_cut will stay False and exit loop
+        for n in list(G.nodes()):
+            if G.in_degree(n) == 1 and G.out_degree(n) == 1:
+                parent = list(G.predecessors(n))[0]
+                child  = list(G.successors(n))[0]
+
+                # accumulate length
+                new_len = G[parent][n]['length'] + G[n][child]['length']
+
+                # reconnect
+                G.add_edge(parent, child, length=new_len)
+
+                # remove middle node
+                G.remove_node(n)
+                more_to_cut = True
+                break
 
     return G
 
@@ -317,71 +422,27 @@ is_right = True
 
 fly = 1
 
-sides = []
-
 while fly < 29:
-    
-    new_side = []
-    
-    side = "R" if is_right else "L"
-            
-    G = trace_file_to_G(f"data/traces_L3/{fly}_Tr9{side}.traces")
-    
-    # for analysis, flip x values to positive (reflect across y axis)
-    # this flipping is not done right, I think
-    mod_G = copy.deepcopy(G)
-    for node in mod_G.nodes():
-        new_side.append(G.nodes[node]["radius"])
         
-
-    coords = TMD(mod_G, min(list(mod_G.nodes())))
-    
+    # side = "R" if is_right else "L"
+    side = "L"
+            
+    G, cut_G = TFG(f"data/traces_L3/{fly}_Tr9{side}.traces")     
+    coords = TMD(G, min(list(cut_G.nodes())))
+        
+    # y axis is in increasing length of strand for persistence barcode
+    TMD_coords = sort_persistence_pairs(coords)
+            
     # the upper left hand drawing will reflect L/R
     # the coordinates will be only the 'reflected to face the right' versions
-    color_plot_walk(G, coords, fly, side)
+    show_data(G, cut_G, TMD_coords, fly, side)
     
     if not is_right: # if we are at left, now we can uptick, since we need fly = 1 for R and L
         fly += 1
     
     is_right = not is_right
-    
-    sides.append(new_side)
-            
-    if fly == 2: # only do the first five for now
-        break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                
+    break
 
 
 
@@ -395,112 +456,6 @@ while fly < 29:
 
 
 #%% Testing
-
-def give_axes(coords, barcode=False):
-    
-    if barcode:
-        max_y = 0
-        
-        for x, y in coords:
-            max_y = y if y > max_y else max_y
-            
-            
-        return [0, max_y + 1, 0, len(coords) + 1]
-        
-    else:
-        max_num = 0
-        
-        for x, y in coords:
-            max_num = x if x > max_num else max_num
-            max_num = y if y > max_num else max_num
-            
-        return [0, max_num + 1, 0, max_num + 1]
-    
-def sort_persistence_pairs(coords):
-    """
-    Sorts (birth, death) pairs by:
-    1) decreasing persistence length
-    2) increasing coordinate magnitude
-    """
-    
-    return sorted(coords, key=lambda x: (-(x[1] - x[0]), x[0] + x[1]))
-
-def color_plot_walk(G, TMD_coords, fly, side):
-
-    savename = f"traces/TMD_{fly}_{side}"
-    fig, axs = plt.subplots(2, 2)
-    
-    ax1 = axs[0, 0]
-    ax2 = axs[0, 1]
-    ax3 = axs[1, 0]
-    ax4 = axs[1, 1]
-    ax4.axis("off")
-    ax1.text(0.05, 0.95, f'{fly}_{side}', transform=ax1.transAxes, fontsize=17, va='center_baseline', ha='center')
-    
-    maxLevel = max(G.nodes()) + 1
-    palette = sns.dark_palette("red", maxLevel, reverse=True)
-
-    for e in G.edges(data=True):
-        # e is a tuple that looks like this:
-        # (node_connected_by_edge, other_node_connected_by_edge, dict_of_attributes)
-        # the dictionary holds level, length, and ange information
-        
-        # find the coordinates of the two nodes connected by this edge
-        c0 = G.nodes[e[0]]['coords']
-        c1 = G.nodes[e[1]]['coords']
-
-        # the level gives index for RGB value
-        c = palette[e[0]]
-
-        ax1.plot([c0[0], c1[0]], [c0[1], c1[1]], color=c, linewidth = 2)
-
-    #plt.axis('equal')
-    #plt.axis('off')
-
-    trace_coords = np.array([i[:2] for i in nx.get_node_attributes(G, 'coords').values()])
-    xs = trace_coords[:, 0]
-    ys = trace_coords[:, 1]
-
-    xcent = 0.5*(np.max(xs) + np.min(xs))
-    ycent = 0.5*(np.max(ys) + np.min(ys))
-
-    lim = 300
-
-    ax1.axis([xcent - lim, xcent + lim, ycent - lim, ycent + lim])
-    plt.gca().set_aspect('equal', adjustable='box')
-    ax1.axis('off')
-    
-    # for the persistence diagram
-    ax2.axis(give_axes(TMD_coords)) # add a formatting function here that dynamically chooses axes
-    ax2.axis("on")
-    ax2.set_xlabel("Birth (distance from root)")
-    ax2.set_ylabel("Death (distance from root)")
-    ax2.set_xticks(np.linspace(0, 6, 7))
-    ax2.set_yticks(np.linspace(0, 6, 7))
-    ax2.grid()
-    
-    for x, y in TMD_coords:
-        ax2.scatter(x, y, color="red", s=10)
-        
-    # persistence barcode
-    ax3.axis(give_axes(TMD_coords, barcode=True)) # add a formatting function here that dynamically chooses axes
-    ax3.axis("on")
-    ax3.set_xlabel("Lifetime (distance from root)")
-    ax3.set_ylabel("Length of Lifetime")
-    ax3.set_xticks(np.linspace(0, 6, 7))
-    ax3.set_yticks(np.linspace(0, 6, 7))
-    ax3.grid()
-    
-    # y axis is in increasing length of strand
-    TMD_coords = sort_persistence_pairs(TMD_coords)
-
-    for index in range(len(TMD_coords)):
-        birth, death = TMD_coords[index]
-        ax3.hlines(y=index + 1, xmin=birth, xmax=death, color='red')
-
-    plt.savefig(savename + '.pdf', bbox_inches='tight')
-    plt.close()
-
 
 k = nx.DiGraph()
 for i in range(1, 11):
@@ -544,7 +499,9 @@ nx.set_node_attributes(k, dict(zip(list(k.nodes()), coords)), "coords")
 
 #%%
 TMD_coords = TMD(k, min(list(k.nodes())))
-color_plot_walk(k, TMD_coords, "test", "test")
+show_data(k, k, TMD_coords, "test", "test")
+
+
 
 
 
